da0850d2becbfa13f07a7dc9f5aa330c7bfee62b
Kai Buijk <120064546+CodingKittyCat@users.noreply.github.com>
2024-09-02 09:46:15 +0200

    Added character name input & dialog
    - Added a method that asks you your character's name
    - Added a method for dialog so you can customise the colour of the line

--- e1_textbased_adventure/Program.cs
+++ e1_textbased_adventure/Program.cs
@@ -8,6 +8,8 @@ namespace Avoniath
 {
     class Game
     {
+        static string characterName = "";
+
         public static void StartGame()
         {
             Console.WriteLine("Welcome to the whimsical region of Avoniath!\nA text-based adventure game set in a medieval world where magic is banished from the kingdom.\nYou're a talented sorcerer, aiming to assassinate the king to end his tyranny and bring magic back to the realm.\n");
@@ -15,6 +17,30 @@ namespace Avoniath
             Console.WriteLine("2. Load Game");
             Console.WriteLine("3. Credits");
             Console.WriteLine("4. Exit");
+        }
+
+        static void NameCharacter()
+        {
+            Dialog("You wouldn't be much of a sorcerer without a proper name, would you? Try to think of a fun name or title!", "blue");
+            characterName = Console.ReadLine();
+            Console.WriteLine($"Your sorcerer's name is now {characterName}");
+        }
+
+        static void Dialog(string message, string colour)
+        {
+            if (colour== "red")
+            { Console.ForegroundColor = ConsoleColor.Red; }
+            else if (colour == "green")
+            { Console.ForegroundColor = ConsoleColor.Green; }
+            else if (colour == "yellow")
+            { Console.ForegroundColor = ConsoleColor.Yellow; }
+            else if (colour == "blue")
+            { Console.ForegroundColor = ConsoleColor.DarkBlue; }
+            else
+            { Console.ForegroundColor = ConsoleColor.White; }
+
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
     class Program
