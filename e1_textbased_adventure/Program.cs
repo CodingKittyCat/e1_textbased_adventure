1b368208528bea503661133531c58fd5c9f70196
Kai Buijk <120064546+CodingKittyCat@users.noreply.github.com>
2024-09-02 09:51:47 +0200

    Added choices for start menu
    - Made a functional choice menu

--- e1_textbased_adventure/Program.cs
+++ e1_textbased_adventure/Program.cs
@@ -17,39 +17,71 @@ namespace Avoniath
             Console.WriteLine("2. Load Game");
             Console.WriteLine("3. Credits");
             Console.WriteLine("4. Exit");
-        }
+            string input = Console.ReadLine();
+            MakeMenuChoice(input);
 
-        static void NameCharacter()
-        {
-            Dialog("You wouldn't be much of a sorcerer without a proper name, would you? Try to think of a fun name or title!", "blue");
-            characterName = Console.ReadLine();
-            Console.WriteLine($"Your sorcerer's name is now {characterName}");
-        }
+            static void NameCharacter()
+            {
+                Dialog("You wouldn't be much of a sorcerer without a proper name, would you? Try to think of a fun name or title!", "blue");
+                characterName = Console.ReadLine();
+                Console.WriteLine($"Your sorcerer's name is now {characterName}");
+            }
 
-        static void Dialog(string message, string colour)
-        {
-            if (colour== "red")
-            { Console.ForegroundColor = ConsoleColor.Red; }
-            else if (colour == "green")
-            { Console.ForegroundColor = ConsoleColor.Green; }
-            else if (colour == "yellow")
-            { Console.ForegroundColor = ConsoleColor.Yellow; }
-            else if (colour == "blue")
-            { Console.ForegroundColor = ConsoleColor.DarkBlue; }
-            else
-            { Console.ForegroundColor = ConsoleColor.White; }
+            static void Dialog(string message, string colour)
+            {
+                if (colour== "red")
+                { Console.ForegroundColor = ConsoleColor.Red; }
+                else if (colour == "green")
+                { Console.ForegroundColor = ConsoleColor.Green; }
+                else if (colour == "yellow")
+                { Console.ForegroundColor = ConsoleColor.Yellow; }
+                else if (colour == "blue")
+                { Console.ForegroundColor = ConsoleColor.DarkBlue; }
+                else
+                { Console.ForegroundColor = ConsoleColor.White; }
 
-            Console.WriteLine(message);
-            Console.ResetColor();
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+
+            static void MakeMenuChoice(string input)
+            {
+                if (input == "1")
+                {
+                    Console.Clear();
+                    NameCharacter();
+                    // Create Save File
+                }
+                else if (input == "2")
+                {
+                    // If save file exists, load game
+                    Console.WriteLine("Load");
+                }
+
+                else if (input == "3")
+                {
+                    // Credits
+                    Console.WriteLine("Creds");
+                }
+
+                else if (input == "4")
+                {
+                    Console.Clear();
+                    Dialog("See you next time!", "red");
+                    System.Environment.Exit(1);
+                }
+
+
+            }
         }
-    }
-    class Program
-    {
-        static void Main()
+        class Program
         {
-            Game.StartGame();
-            Console.ReadKey();
-         
+            static void Main()
+            {
+                Game.StartGame();
+                Console.ReadKey();
+
+            }
         }
     }
 }
