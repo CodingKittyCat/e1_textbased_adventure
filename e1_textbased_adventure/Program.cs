04a6422956184e5e5a7a5e9f1bae6ab8437fdda6
Kai Buijk <120064546+CodingKittyCat@users.noreply.github.com>
2024-09-02 09:41:13 +0200

    Added Classes and a begin screen
    - Added an introduction screen when you launch the program
    - Added a Game class to indicate that it's a game
    - Added a method StartGame() to start the game

--- e1_textbased_adventure/Program.cs
+++ e1_textbased_adventure/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace Avoniath
+{
+    class Game
+    {
+        public static void StartGame()
+        {
+            Console.WriteLine("Welcome to the whimsical region of Avoniath!\nA text-based adventure game set in a medieval world where magic is banished from the kingdom.\nYou're a talented sorcerer, aiming to assassinate the king to end his tyranny and bring magic back to the realm.\n");
+            Console.WriteLine("1. New Game");
+            Console.WriteLine("2. Load Game");
+            Console.WriteLine("3. Credits");
+            Console.WriteLine("4. Exit");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Game.StartGame();
+            Console.ReadKey();
+         
+        }
+    }
+}
+
+
