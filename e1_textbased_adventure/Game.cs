143b0075656d1666195af2de0c40e0e11d329a78
Kai Buijk <120064546+CodingKittyCat@users.noreply.github.com>
2024-09-02 11:15:01 +0200

    Added Story System
    - Added A Dictionary with placeholder choices
    - Added a Scene class to make scenes for said choices
    - Added methods in Game.cs to accompany choices and connect them

--- e1_textbased_adventure/Game.cs
+++ e1_textbased_adventure/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,9 @@ namespace e1_textbased_adventure
     class Game
     {
         static string characterName = "";
+        float currentScene = 1.0f;
+        Dictionary<float, Scene> story = new Dictionary<float, Scene>();
+       
 
         public static void StartGame()
         {
@@ -27,22 +31,8 @@ namespace e1_textbased_adventure
                 Console.WriteLine($"Your sorcerer's name is now {characterName}");
             }
 
-            static void Dialog(string message, string colour)
-            {
-                if (colour== "red")
-                { Console.ForegroundColor = ConsoleColor.Red; }
-                else if (colour == "green")
-                { Console.ForegroundColor = ConsoleColor.Green; }
-                else if (colour == "yellow")
-                { Console.ForegroundColor = ConsoleColor.Yellow; }
-                else if (colour == "blue")
-                { Console.ForegroundColor = ConsoleColor.DarkBlue; }
-                else
-                { Console.ForegroundColor = ConsoleColor.White; }
 
-                Console.WriteLine(message);
-                Console.ResetColor();
-            }
+            
 
             static void MakeMenuChoice(string input)
             {
@@ -70,10 +60,93 @@ namespace e1_textbased_adventure
                     Dialog("See you next time!", "red");
                     System.Environment.Exit(1);
                 }
+            }
+
+        }
+
+        private void StoryInitialize()
+        {
+            InitializeStory storyInitializer = new InitializeStory();
+            story = storyInitializer.StoryInitialize();
+        }
 
 
+        void DisplayScene()
+        {
+            Console.Clear();
+            foreach (char character in story[currentScene].Text)
+            {
+                Dialog(character, "blue");
+                Thread.Sleep(60);
+            }
+            Console.WriteLine("/n ");
+            int choiceIndex = 0;
+
+            foreach (Choice choice in story[currentScene].Choices)
+            {
+                choiceIndex++;
+                Console.WriteLine($"{choiceIndex}. {choice.Option}"); 
+            }
+        }
+        float GetPlayerChoice()
+        {
+            Console.Write("/nEnter your choice: ");
+            int choiceIndex;
+
+            while (!int.TryParse(Console.ReadLine(), out choiceIndex) || choiceIndex < 1 || choiceIndex > story[currentScene].Choices.Count)
+            {
+                Console.WriteLine("invalid input. Please enter a valid option.");
+                Console.Write("Enter your choice: ");
+            }
+
+            return story[currentScene].Choices[choiceIndex - 1].NextScene;
+        }
+
+        void UpdateScene(float choice)
+        {
+            if (choice >= 1.0f && choice <= 5.0f)
+            {
+                currentScene = choice;
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please choose a valid option.");
             }
         }
 
+
+        public static void Dialog(string message, string colour)
+        {
+            if (colour== "red")
+            { Console.ForegroundColor = ConsoleColor.Red; }
+            else if (colour == "green")
+            { Console.ForegroundColor = ConsoleColor.Green; }
+            else if (colour == "yellow")
+            { Console.ForegroundColor = ConsoleColor.Yellow; }
+            else if (colour == "blue")
+            { Console.ForegroundColor = ConsoleColor.DarkBlue; }
+            else
+            { Console.ForegroundColor = ConsoleColor.White; }
+
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void Dialog(char character, string colour)
+        {
+            if (colour== "red")
+            { Console.ForegroundColor = ConsoleColor.Red; }
+            else if (colour == "green")
+            { Console.ForegroundColor = ConsoleColor.Green; }
+            else if (colour == "yellow")
+            { Console.ForegroundColor = ConsoleColor.Yellow; }
+            else if (colour == "blue")
+            { Console.ForegroundColor = ConsoleColor.DarkBlue; }
+            else
+            { Console.ForegroundColor = ConsoleColor.White; }
+
+            Console.Write(character);
+            Console.ResetColor();
+        }
     }
 }
---
+++ e1_textbased_adventure/InitializeStory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace e1_textbased_adventure
+{
+    internal class InitializeStory
+    {
+        public Dictionary<float, Scene> StoryInitialize()
+        {
+            Dictionary<float, Scene> story = new Dictionary<float, Scene>();
+            story.Add(1.0f, new Scene("Story 1",
+
+            new List<Choice>
+            {
+                new Choice("Choice 1", 1.1f),
+                new Choice("Choice 2", 1.2f)
+            }));
+
+            return story;
+        }
+
+    }
+    class Scene
+    {
+        public string Text { get; }
+        public List<Choice> Choices { get; }
+
+        public Scene(string text, List<Choice> choices)
+        {
+            Text = text;
+            Choices = choices;
+        }
+    }
+
+    class Choice
+    {
+        public string Option { get; }
+        public float NextScene { get; }
+
+        public Choice(string option, float nextScene) 
+        {
+            Option = option;
+            NextScene = nextScene;  
+        }
+}
