3d913d12db69af9c2a873c81071939234b28a628
Kai Buijk <120064546+CodingKittyCat@users.noreply.github.com>
2024-09-09 11:04:04 +0200

    Added more story choices

--- e1_textbased_adventure/InitializeStory.cs
+++ e1_textbased_adventure/InitializeStory.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 
 namespace e1_textbased_adventure
 {
@@ -11,36 +7,76 @@ namespace e1_textbased_adventure
     {
         public Dictionary<float, Scene> StoryInitialize()
         {
+
+             
             Dictionary<float, Scene> story = new Dictionary<float, Scene>();
-            story.Add(1.0f, new Scene("Story 1",
+            // Story Template
+            //story.Add(int, new Scene("Story",
+
+            //   new List<Choice>
+            //   {
+            //        new Choice("Choice 1", float),
+            //        new Choice("CHoice 2", float),
+            //   }
+            //   ));
+            story.Add(1.0f, new Scene(
+                "You find yourself wandering in a vibrant, lush forest at night. Gigantic trees of green tower out above you. Spread inbetween these trees are big, blue, glowing mushrooms with strands hanging down on them. As a sorcerer, you know that these mushrooms can be highly poisonous for certain individuals, including yourself.\nWhat do you do?"
+                ,
 
             new List<Choice>
             {
-                new Choice("Choice 1", 1.1f),
-                new Choice("Choice 2", 1.2f)
-            }));
+                new Choice("I harvest a sample of the mushroom.", 1.1f),
+                new Choice("I leave the mushroom and continue on my way.", 1.2f)
+            },
 
-            story.Add(1.1f, new Scene("Story 1.1",
+            "Test Instructions"));
+
+            // 1.1 Story
+            story.Add(1.1f, new Scene("You take the risk, not knowing if you're poisonous to it or not. As you start harvesting a sample from the glowing mushroom, you notice your knees begin to feel weak. Your eyes turn blurry, the world around you starts spinning, and your brain feels hazy. You manage to put it in your pouch, and you begin to start feeling better, albeit still feeling weak. What do you do?",
 
             new List<Choice>
             {
-                new Choice("Choice 1", 1.3f),
-                new Choice("Choice 2", 1.4f)
-            }));
+                new Choice("I use my magic to make camp, and take a breather. The mushroom really did a number on me and it is wise to rest up.", 1.11f),
+                new Choice("I push on, feeling weak but determined to carry out my plans to assassinate the king.", 1.12f)
+            },
+            
+            "Test Instructions no.2"));
 
+            story.Add(1.11f, new Scene("You begin to gather sticks and stones to make a campfire. ",
+
+               new List<Choice>
+               {
+                    new Choice("Choice 1", 1.13f),
+                    new Choice("CHoice 2", 1.14f),
+               },
+
+               "Test Instructions no.3"
+               ));
+
+            story.Add(1.2f, new Scene("Story",
+
+               new List<Choice>
+               {
+                    new Choice("Choice 1", 1.5f),
+                    new Choice("CHoice 2", 1.6f),
+               },
+               "test Instruct"
+               ));
             return story;
         }
-
     }
+
     class Scene
     {
         public string Text { get; }
         public List<Choice> Choices { get; }
+        public string Instructions { get; }
 
-        public Scene(string text, List<Choice> choices)
+        public Scene(string text, List<Choice> choices, string instructions)
         {
             Text = text;
             Choices = choices;
+            Instructions = instructions;
         }
     }
 
